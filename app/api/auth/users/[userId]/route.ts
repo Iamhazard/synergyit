a53846acaebf7c6ccc93c4ca6edbfe48dabbff7d
Amrit Acharya <Amrithazard@gmail.com>
2024-08-09 15:07:34 +0545

    //

--- app/api/auth/users/[userId]/route.ts
+++ app/api/auth/users/[userId]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server";
 interface IParams {
     userId?: string;
 }
-export async function GET(req: NextRequest, { params }: { params: IParams }) {
+export const DELETE = async (req: NextRequest, { params }: { params: IParams }) => {
     const { userId } = params;
     if (req.method === "GET") {
         try {
@@ -18,18 +18,15 @@ export async function GET(req: NextRequest, { params }: { params: IParams }) {
             if (!currentUser) {
                 return new Response("User not found", { status: 404 });
             }
-            return new Response(JSON.stringify(currentUser), { status: 200 });
+
+            const deleteUser = await db.category.delete({
+                where: { id: userId }
+            })
+            return new Response(JSON.stringify(deleteUser), { status: 200 });
         } catch (error) {
             console.error(error);
             return new Response("Internal Server Error", { status: 500 });
         }
-    } else if (req.method == 'DELETE') {
-
-        const deleteUser = await db.category.delete({
-            where: { id: userId }
-        })
-        return new Response(JSON.stringify(deleteUser), { status: 200 });
-
     }
 
 }
