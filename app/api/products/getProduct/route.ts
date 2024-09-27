76fc513946f23f868f3e5ac2e0a6240ec3943325
Amrit Acharya <Amrithazard@gmail.com>
2024-09-27 15:14:38 +0545

    changes

--- app/api/products/getProduct/route.ts
+++ app/api/products/getProduct/route.ts
@@ -5,8 +5,8 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
 
     if (req.method == "GET") {
         try {
-            const newAllCategoey = await db.product.findMany()
-            return new Response(JSON.stringify(newAllCategoey), { status: 201 });
+            // const newAllCategoey = await db.product.findMany()
+            // return new Response(JSON.stringify(newAllCategoey), { status: 201 });
 
         } catch (error) {
             console.log(error)
